d1462337d0498717fb1d835f15f3f2ae02f7a01b
Corey Gumbs <5513859+CoreyGumbs@users.noreply.github.com>
2020-01-21 19:13:11 -0500

    [Add] Created todoReducer and initialState, imported to App component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 
+import {initialState, todoReducer} from './reducers/reducers';
+
+import TodoList from './components/TodoList/TodoList';
+import TodoForm from './components/TodoForm/TodoForm';
 import './App.css';
 
+
 function App() {
+  console.log(initialState);
   return (
     <div className="App">
       <TodoList />
-      <TodoItem />
-      <TodoForm />
+      <TodoForm />  
     </div>
   );
 }
---
+++ src/reducers/reducers.js
@@ -0,0 +1,9 @@
+export const initialState = [{
+    item: '',
+    completed: false,
+    id: Date.now()
+}];
+
+export const todoReducer = (currentState, action) => {
+    return currentState;
+}
